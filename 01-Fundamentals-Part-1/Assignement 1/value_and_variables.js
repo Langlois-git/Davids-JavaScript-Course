0fa889a25ccf4039690008aed4e59b5783679900
Langlois-git <142052623+Langlois-git@users.noreply.github.com>
2025-06-05 21:19:01 -0600

    Completion of assignment 1

---
+++ 01-Fundamentals-Part-1/Assignement 1/value_and_variables.js
@@ -0,0 +1,7 @@
+let country = 'Canada';
+let continent = 'North America';
+let population = '48 million';
+
+console.log('You live in ' + country + '.');
+console.log('Which is contained in the continent of ' + continent + '.');
+console.log('The population of ' + country + ' is ' + population + '.');
